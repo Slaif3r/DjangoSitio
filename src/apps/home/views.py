f5ffe60310017b4641708d0cc07aefc024869303
Aristides A. Morcillo <amorcillo@proyecto.com>
2017-12-08 14:01:10 -0500

    Inicia el proyecto

---
+++ src/apps/home/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+
+class Attributes(TemplateView):
+    template_name = "tooltip.html"
+
---
+++ src/apps/parcial2/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Parcial2Config(AppConfig):
+    name = 'parcial2'
