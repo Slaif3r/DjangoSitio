5f4684d0105d3bb75c869d1b0a3b2878e7451d07
Slaif3r <aristides03_30@hotmail.com>
2017-12-13 19:39:13 -0500

    Se realiza base de datos
    se genera  tablas en la base de datos

--- src/apps/semestral/admin.py
+++ src/apps/semestral/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from apps.semestral.models import Profesor, Estudiante, Asignatura, Notas
+
 # Register your models here.
+admin.site.register(Profesor)
+admin.site.register(Estudiante)
+admin.site.register(Asignatura)
+admin.site.register(Notas)
